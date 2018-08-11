cc80cd8ceaa8f566744e68dd7bf268ab72b61af5
Dan Langer <daniel@langer.me>
2018-08-11 00:26:05 -0400

    Do JS client-side maths

--- static/form.js
+++ static/form.js
@@ -1,28 +1,68 @@
 (function () {
-    const downloadFilename = (advanceForm) => {
+    const downloadFilename = advanceForm => {
         const today = new Date();
         const year = today.getFullYear();
         const month = ("0" + String(today.getMonth() + 1).slice(-2));
         const day = ("0" + today.getDate()).slice(-2);
-
         return year + month + day + " Advance - " + advanceForm.travel_city;
     };
 
-    $(document).ready(() => {
-        $('#submit-expenses').on('mdl-componentupgraded', () => {
-            const submitButton = $("#submit-expenses");
+    const dateFromString = dateString => {
+        const dateParts = dateString.split('-');
+        const year = dateParts[0];
+        const month = parseInt(dateParts[1], 10) - 1;
+        const day = dateParts[2];
+        return new Date(year, month, day);
+    };
 
-            $("#submit-expenses").click(() => {
+    const dateDiff = (first, second) => Math.round((second-first)/(1000*60*60*24)); // Round to "handle" DST 
+    
+    const changeMdlTextField = (jqObject, val) => jqObject.get(0).parentElement.MaterialTextfield.change(val);
+
+    $(document).ready(() => {
+        const submitButton = $("#submit_expenses");
+        
+        submitButton.on('mdl-componentupgraded', () => {    
+            const depDateField = $("#dep_date");
+            const retDateField = $("#ret_date");
+            const bfastField = $('#num_breakfasts');
+            const lunchField = $('#num_lunches');
+            const dinnerField = $('#num_dinners');
+            const hotelNightsField = $('#hotel_nights');
+
+            const dateChangeHandler = () => {
+                if ((depDateField.val() == "") || (retDateField.val() == ""))
+                    return;
+
+                const depDate = dateFromString(depDateField.val());
+                const retDate = dateFromString(retDateField.val());
+                const tripDuration = dateDiff(depDate, retDate);
+
+                if (tripDuration !== tripDuration) // Check for NaN, which happens if either value is invalid
+                    return;
+                    
+                if (tripDuration < 0)
+                    return;
+
+                changeMdlTextField(hotelNightsField, tripDuration);
+                changeMdlTextField(bfastField, tripDuration + 1);
+                changeMdlTextField(lunchField, tripDuration + 1);
+                changeMdlTextField(dinnerField, tripDuration + 1);                
+            }
+
+            depDateField.change(dateChangeHandler);
+            retDateField.change(dateChangeHandler);
+
+            submitButton.click(() => {
                 const formElements =$("#advance-form input[type=text]");
                 let data = {}; // FIXME to const
                 let errorFlag = false;
                 
                 for (element of formElements) {
                     if (element.value == "") {
-                        // alert("Error! All fields must have a value.");
-                        // errorFlag = true;
-                        // break;
-                        // FIXME
+                        alert("Error! All fields must have a value.");
+                        errorFlag = true;
+                        break;
                     }
                     data[element.id] = element.value;
                 }
@@ -46,8 +86,7 @@
                     //     "hotel_nights":"2",
                     //     "rental_amount":"160.99",
                     //     "transport_amount":"60"
-                    // } // FIXME
-
+                    // } 
                     // data = fakeData; // FIXME
 
                     const request = new XMLHttpRequest();
@@ -55,7 +94,7 @@
                     request.setRequestHeader("Content-Type", "application/json");
                     request.responseType = "blob";
 
-                    request.addEventListener('load', (event) => {
+                    request.addEventListener('load', event => {
                         if (request.status == 200) {
                             const blob = request.response;
                             const link = document.createElement('a');
